cb514b9c075a83da8510fd42aba3a87a6b372726
Wilson Birch <wilsonbirch@gmail.com>
2020-08-25 14:48:52 -0400

    updating for jawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host:"localhost",
-    port: 3306,
-    user: "root",
-    password: "3Strong13!",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host:"localhost",
+        port: 3306,
+        user: "root",
+        password: "3Strong13!",
+        database: "burgers_db"
+    });
+}
+
   
 connection.connect(function(err) {
     if (err) {
